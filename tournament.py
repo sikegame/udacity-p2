c1528f2e5e5b151a05bd9ac4cfd1c5a92fc0467d
Shinsuke JJ Ikegame <shinsuke@ikega.me>
2015-09-25 15:54:01 +0700

    Fix some PEP8 related errors

--- tournament.py
+++ tournament.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# 
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
+
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -40,10 +40,10 @@ def countPlayers():
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
@@ -59,8 +59,8 @@ def registerPlayer(name):
 def playerStandings():
     """Returns a list of the players and their win records, sorted by wins.
 
-    The first entry in the list should be the player in first place, or a player
-    tied for first place if there is currently a tie.
+    The first entry in the list should be the player in first place,
+    or a player tied for first place if there is currently a tie.
 
     Returns:
       A list of tuples, each of which contains (id, name, wins, matches):
@@ -94,16 +94,16 @@ def reportMatch(winner, loser, draw=False):
                loser, winner, False, ))  # Insert the loser into
     db.commit()
     db.close()
- 
- 
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
